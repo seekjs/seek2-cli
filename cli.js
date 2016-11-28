4f1f80f32dcd8a0ef41bd140fdf525f4d77b6d15
利开团 <likaituan@wecash.net>
2016-11-28 16:47:00 +0800

    quick submit

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@
 var fs = require("fs");
 var path = require("path");
 var {getArgs,log,end,cmd,requireJson} = require("ifun");
-var pk = require("./package.json");
+var {name,version} = require("./package.json");
 
 var args = getArgs("cmd");
 var ua = {};
